d620377458ff2e00cc26aeabca330add1ceed15f
Bugs0007 <150580785+Bugs0007@users.noreply.github.com>
2024-06-27 23:36:38 +0530

    Update Report.py

--- pages/Report.py
+++ pages/Report.py
@@ -159,7 +159,7 @@ import datetime
 
 def run_report_page(face_rec):
     # Set page title and layout
-    st.set_page_config(page_title='Attendance Reporting', layout='wide')
+    #st.set_page_config(page_title='Attendance Reporting', layout='wide')
     st.title('Attendance Reporting')
 
     # Function to retrieve logs from Redis
