bae385211dbfaefbbb985088946aeda08a794e6b
Bugs0007 <150580785+Bugs0007@users.noreply.github.com>
2024-06-27 23:28:05 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -55,7 +55,10 @@
 # Home.py
 
 import streamlit as st
-from face_rec import retrive_data  # Corrected import statement
+from auth import authenticate
+from pages.Real_Time_Prediction import run_realtime_prediction_page
+from pages.Registration_form import run_register_page
+from pages.Report import run_report_page
 
 # Check if the user is authenticated
 if 'authenticated' not in st.session_state:
@@ -71,7 +74,7 @@ else:
     if page_selection == 'Real-Time Prediction':
         run_realtime_prediction_page()
     elif page_selection == 'Registration Form':
-        run_registration_form_page()
+        run_register_page()
     elif page_selection == 'Attendance Report':
         run_report_page()
 
