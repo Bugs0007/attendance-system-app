2297f94e8d3d3958c1375ea2295755b8336392b8
Bugs0007 <150580785+Bugs0007@users.noreply.github.com>
2024-06-27 20:30:47 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from auth import authenticate 
+from auth import authenticate
+import face_rec  # Assuming face_rec is a module or function defined elsewhere
 
 # Check if the user is authenticated
 if 'authenticated' not in st.session_state:
@@ -11,11 +12,10 @@ else:
     st.set_page_config(page_title='Attendance System', layout='wide')
 
     with st.spinner("Initializing face recognition components"):
-        import face_rec  
-
-    st.header('Attendance System using Face Recognition')
-    st.success('Model loaded successfully')
-    st.success('Redis db successfully connected')
+        # Importing face_rec here assumes it's correctly defined and accessible
+        st.header('Attendance System using Face Recognition')
+        st.success('Model loaded successfully')
+        st.success('Redis db successfully connected')
 
     if st.sidebar.button("Logout"):
         st.session_state['authenticated'] = False
