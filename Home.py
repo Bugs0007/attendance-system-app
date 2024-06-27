62216a709dcc6be2f0a8e6be9656cff723018266
Bugs0007 <150580785+Bugs0007@users.noreply.github.com>
2024-06-27 19:53:55 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -20,7 +20,7 @@ else:
     if st.sidebar.button("Logout"):
         st.session_state['authenticated'] = False
         st.experimental_rerun()
-///////
+
 from flask import Flask, render_template, redirect, url_for, session
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
