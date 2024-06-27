50ebcb26b81f2d7ba9487314cac568b0f63df51b
Bugs0007 <150580785+Bugs0007@users.noreply.github.com>
2024-06-27 22:45:59 +0530

    Update Real_Time_Prediction.py

--- pages/Real_Time_Prediction.py
+++ pages/Real_Time_Prediction.py
@@ -60,41 +60,33 @@ def run_realtime_prediction_page():
 
     # Retrieve the data from Redis Database
     with st.spinner('Retrieving Data from Redis DB ...'):
-        redis_face_db = face_rec.retrive_data(name='academy:register')
+        redis_face_db = face_rec.retrieve_data(name='academy:register')
         st.dataframe(redis_face_db)
 
     st.success("Data successfully retrieved from Redis")
 
-    # Initialize variables for real-time prediction
+    # Time settings
     waitTime = 5  # time in seconds
     setTime = time.time()
-    realtimepred = face_rec.RealTimePred()  # Assuming RealTimePred is defined in face_rec
+    realtimepred = face_rec.RealTimePred()  # real-time prediction class
 
-    # Define the callback function for webrtc_streamer
+    # Real-Time Prediction
     def video_frame_callback(frame):
         nonlocal setTime
 
-        img = frame.to_ndarray(format="bgr24")  # Convert frame to numpy array (bgr24 format)
-        
-        # Perform face prediction using the real-time prediction class
-        pred_img = realtimepred.face_prediction(img, redis_face_db,
-                                                'facial_features', ['Name', 'Role'], thresh=0.5)
+        img = frame.to_ndarray(format="bgr24")  # 3-dimensional numpy array
+        pred_img = realtimepred.face_prediction(img, redis_face_db, 'facial_features', ['Name', 'Role'], thresh=0.5)
 
-        # Manage time for saving logs periodically
         timenow = time.time()
         difftime = timenow - setTime
         if difftime >= waitTime:
             realtimepred.saveLogs_redis()
-            setTime = time.time()  # Reset time for next interval
-            print('Saved Data to redis database')
+            setTime = time.time()  # reset time
+            print('Save Data to redis database')
 
-        # Return processed image as av.VideoFrame for webrtc_streamer
         return av.VideoFrame.from_ndarray(pred_img, format="bgr24")
 
-    # Initialize webrtc_streamer with defined callback function and RTC configuration
     webrtc_streamer(key="realtimePrediction", video_frame_callback=video_frame_callback,
                     rtc_configuration={
                         "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
                     })
-
-
