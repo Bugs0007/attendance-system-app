5a2ce8b365cef83f5e14445163bb723fc6e88340
Bugs0007 <150580785+Bugs0007@users.noreply.github.com>
2024-07-16 18:32:58 +0530

    Update upload_logs.py

--- upload_logs.py
+++ upload_logs.py
@@ -1,10 +1,12 @@
 import redis
 
 # Connect to Redis Client
-hostname = 'redis-18195.c301.ap-south-1-1.ec2.redns.redis-cloud.com'
-portnumber = 18195
-password = 'XThqqtHKksOod1ICKRkhPoeVAufi0DUB'
-
+# hostname = 'redis-18195.c301.ap-south-1-1.ec2.redns.redis-cloud.com'
+# portnumber = 18195
+# password = 'XThqqtHKksOod1ICKRkhPoeVAufi0DUB'
+hostname = 'redis-18549.c322.us-east-1-2.ec2.redns.redis-cloud.com'
+portnumber = 18549
+password = '0CMhOIkl2gHDzNWiMnIvPgEtDpDhBheB'
 r = redis.StrictRedis(host=hostname,
                       port=portnumber,
                       password=password)
